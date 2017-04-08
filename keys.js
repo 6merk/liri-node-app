2064227072b81bf0310df9905272f44e92d89752
Mark Kahler <kahler6@gmail.com>
2017-04-07 18:52:13 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ console.log('this is loaded');
 exports.twitterKeys = {
   consumer_key: '<tdIPxcDHGGDgpNfBNCRpTE6tx>',
   consumer_secret: '<0Q903hkQI6f2NnEkX11YsFOZslG5ZVHxoRKaeGfZHKl20bxBJQ>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  access_token_key: '<480661909-AAEtseizfxBMk8mVXXJqOTGFluOVyQf2iVDIJxNd>',
+  access_token_secret: '<mLA7hcYcXjA1ELd9FiZjDKNVfTazyomyqQoJXm0frQemI>',
 }
